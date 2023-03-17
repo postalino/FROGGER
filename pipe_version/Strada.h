8bd2f22338d3fe00296612977dd1ff28ed1f8942
postalino <davidesenette01@gmail.com>
2023-03-17 18:30:02 +0100

    strada 1.2

--- pipe_version/Strada.h
+++ pipe_version/Strada.h
@@ -5,9 +5,9 @@
 
 #define N_VEICOLI 6
 
-#define STRADA_1 25
-#define STRADA_2 29
-#define STRADA_3 33
+#define STRADA_1 24
+#define STRADA_2 28
+#define STRADA_3 32
 
 //prototipo funzione per generare i 6 diversi processi dei 6 veicoli
 void genera_processi_veicoli(int[N_VEICOLI][2],pid_t[N_VEICOLI]);
